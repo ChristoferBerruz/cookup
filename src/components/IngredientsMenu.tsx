f32519b6f01649b33928855496cae761d1a384d5
Christofer Berruz <cberruzc@my.bridgeport.edu>
2020-10-30 20:43:17 -0400

    Modals for recipes. Still hardcoded

--- src/components/IngredientsMenu.tsx
+++ src/components/IngredientsMenu.tsx
@@ -1,32 +1,74 @@
 import React, {Dispatch, useState} from 'react';
-import {IonContent, IonHeader, IonGrid, 
-    IonRow, IonCol, IonSearchbar, IonTitle,
-     IonToolbar, IonSegment, IonItem, IonSegmentButton,
-     IonSelect, IonModal, IonButton, IonList, 
-     IonListHeader, IonLabel, IonSelectOption, IonItemDivider, IonMenu, IonIcon, IonChip} from '@ionic/react';
+import {IonSearchbar, IonTitle, IonItem,
+    IonLabel, IonIcon, IonList, IonHeader,
+    IonToolbar, IonButtons, IonButton, IonModal, IonChip} from '@ionic/react';
 import {restaurant, closeCircle} from 'ionicons/icons';
 import Repository from '../DataLayer/Repository';
 
 interface IngredientSectionProps{
-    sectionName:string
+    sectionName:string,
+    ingredients:Array<string>
 }
 
-const IngredientSection:React.FC<IngredientSectionProps> = ({sectionName}) =>{
+const IngredientSection:React.FC<IngredientSectionProps> = ({sectionName, ingredients}) =>{
+    const [isModalOpen, setModalOpen] = useState(false);
+    const IngredientChips = ingredients.map(ing => {
+        return(
+            <IonChip key={ing!}>
+                <IonLabel>{ing!}</IonLabel>
+            </IonChip>
+        );
+    });
     return(
-        <IonItem button onClick={() => {}}>
-            <IonLabel>
-                {sectionName}
-            </IonLabel>
-        </IonItem>
+        <React.Fragment>
+            <IonModal isOpen={isModalOpen}
+            onDidDismiss={() => setModalOpen(false)}
+            >
+                <form className="ion-padding">
+                    <IonTitle> Select {sectionName}</IonTitle>
+                    {IngredientChips}
+                    <br />
+                    <IonButton onClick={() => setModalOpen(false)}
+                    >
+                        Done
+                    </IonButton>
+                </form>
+            </IonModal>
+            <IonItem
+            button onClick={() => setModalOpen(true)}>
+                <IonLabel>
+                    {sectionName}
+                </IonLabel>
+            </IonItem>
+        </React.Fragment>
     );
 }
 
 const IngredientSearch : React.FC = () => {
     const [searchText, setSearchText] = useState('');
-    const sections = ["Dairy", "Vegetables", "Meat", "Spices"];
-    const Sections = sections.map(
-        sec => <IngredientSection sectionName={sec!}/>
-    );
+    const ingredientsDairy = ["milk", "cheese"];
+    const ingredientsVeg = ["tomatoes", "carrots"];
+    const ingredientsMeat = ["chicken", "beef"];
+    const ingredientsSpices= ["paprika", "onion powder"];
+
+    const sections:any = {
+        "Dairy":ingredientsDairy,
+        "Vegetables":ingredientsVeg,
+        "Meats":ingredientsMeat,
+        "Spices":ingredientsSpices
+    };
+
+    const SectionsComponent = [];
+
+    for(let sec in sections){
+        let ingredients = sections[sec];
+        SectionsComponent.push(
+            <IngredientSection sectionName={sec}
+                ingredients={ingredients}
+                key={sec}
+             />
+        );
+    }
     return(
         <React.Fragment>
             {/** Heading for recipe search */}
@@ -42,7 +84,9 @@ const IngredientSearch : React.FC = () => {
                 onIonChange={e => setSearchText(e.detail.value!)}
             >
             </IonSearchbar>
-            {Sections}
+            <IonList>
+                {SectionsComponent}
+            </IonList>
         </React.Fragment>
     );
 }
